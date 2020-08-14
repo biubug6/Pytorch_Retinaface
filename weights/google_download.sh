90da354e1749db79fcfa4f497455f4da4565851d
Zhiting Zhu <zhitingz@cs.utexas.edu>
2020-08-14 20:49:55 +0000

    fix download script
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- weights/google_download.sh
+++ weights/google_download.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -euo pipefail
+set -euox pipefail
 FILEID=$1
 FILENAME=$2
-wget --load-cookies /tmp/cookies.txt \
-    "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies \
-    /tmp/cookies.txt --keep-session-cookies --no-check-certificate \
-    'https://docs.google.com/uc?export=download&id=$FILEID' -O- | sed -rn \
-    's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
+cookie=$(FILEID=$1 ; wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies \
+    --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | \
+    sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$cookie&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
