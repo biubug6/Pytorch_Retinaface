957a830a219ff9486f746039bc80fb547f4e4e59
Ammad Nadeem <ammad.nadeeem@gmail.com>
2021-02-26 22:20:30 +0500

    comet integration

--- train.py
+++ train.py
@@ -1,7 +1,10 @@
 from __future__ import print_function
+from comet_ml import Experiment
+experiment = Experiment()
 import os
 import torch
 import torch.optim as optim
+import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 import argparse
 import torch.utils.data as data
@@ -12,6 +15,11 @@ import time
 import datetime
 import math
 from models.retinaface import RetinaFace
+from utils.box_utils import decode, decode_landm
+from utils.nms.py_cpu_nms import py_cpu_nms
+import cv2
+from numpy import array as to_numpy_array
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Retinaface Training')
 parser.add_argument('--training_dataset', default='./data/widerface/train/label.txt', help='Training dataset directory')
@@ -34,6 +42,8 @@ if args.network == "mobile0.25":
     cfg = cfg_mnet
 elif args.network == "resnet50":
     cfg = cfg_re50
+experiment.log_others(cfg)
+experiment.log_others(vars(args))
 
 rgb_mean = (104, 117, 123) # bgr order
 num_classes = 2
@@ -42,6 +52,8 @@ num_gpu = cfg['ngpu']
 batch_size = cfg['batch_size']
 max_epoch = cfg['epoch']
 gpu_train = cfg['gpu_train']
+experiment.log_other("RGB Mean",rgb_mean)
+experiment.log_other("Num Classes",num_classes)
 
 num_workers = args.num_workers
 momentum = args.momentum
@@ -86,6 +98,70 @@ with torch.no_grad():
     priors = priorbox.forward()
     priors = priors.cuda()
 
+def push_images(img, priors, loc, conf, landms):
+    resize = 1
+    scale = torch.Tensor([img.shape[3], img.shape[2], img.shape[3],
+                        img.shape[2]]).cuda()
+    conf = F.softmax(conf, dim=-1)
+    prior_data = priors.data
+    boxes = decode(loc.data.squeeze(0), prior_data, cfg['variance'])
+    boxes = boxes * scale / resize
+    boxes = boxes.cpu().numpy()
+    scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+    landms = decode_landm(landms.data.squeeze(0), prior_data, cfg['variance'])
+    scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3],
+                          img.shape[2], img.shape[3], img.shape[2],
+                          img.shape[3], img.shape[2],
+                          img.shape[3], img.shape[2]])
+    scale1 = scale1.cuda()
+    landms = landms * scale1 / resize
+    landms = landms.cpu().numpy()
+
+    # ignore low scores
+    inds = np.where(scores > 0.02)[0]
+    boxes = boxes[inds]
+    landms = landms[inds]
+    scores = scores[inds]
+
+    # keep top-K before NMS
+    order = scores.argsort()[::-1]
+    # order = scores.argsort()[::-1][:args.top_k]
+    boxes = boxes[order]
+    landms = landms[order]
+    scores = scores[order]
+
+    # do NMS
+    dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+    keep = py_cpu_nms(dets, 0.40)
+    # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
+    dets = dets[keep, :]
+    landms = landms[keep]
+
+    dets = np.concatenate((dets, landms), axis=1)
+
+    #Plot
+    img_raw = ((img[0, ...].permute(1, 2, 0).to('cpu').numpy() +
+             np.array([104, 117, 123]))).astype(np.uint8)
+    for b in dets:
+        if b[4] < 0.50:
+            continue
+        text = "{:.4f}".format(b[4])
+        b = list(map(int, b))
+        cv2.rectangle(img_raw, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 2)
+        cx = b[0]
+        cy = b[1] + 12
+        cv2.putText(img_raw, text, (cx, cy),
+                    cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
+
+        # landms
+        cv2.circle(img_raw, (b[5], b[6]), 1, (0, 0, 255), 4)
+        cv2.circle(img_raw, (b[7], b[8]), 1, (0, 255, 255), 4)
+        cv2.circle(img_raw, (b[9], b[10]), 1, (255, 0, 255), 4)
+        cv2.circle(img_raw, (b[11], b[12]), 1, (0, 255, 0), 4)
+        cv2.circle(img_raw, (b[13], b[14]), 1, (255, 0, 0), 4)
+    # save image
+    return torch.from_numpy(to_numpy_array(img_raw[:, :, ::-1]))
+
 def train():
     net.train()
     epoch = 0 + args.resume_epoch
@@ -109,7 +185,10 @@ def train():
             # create batch iterator
             batch_iterator = iter(data.DataLoader(dataset, batch_size, shuffle=True, num_workers=num_workers, collate_fn=detection_collate))
             if (epoch % 10 == 0 and epoch > 0) or (epoch % 5 == 0 and epoch > cfg['decay1']):
-                torch.save(net.state_dict(), save_folder + cfg['name']+ '_epoch_' + str(epoch) + '.pth')
+                torch.save(net.state_dict(), save_folder + cfg['name'] + '_epoch_' + str(epoch) + '.pth')
+                experiment.log_model("RetinaFace", save_folder +
+                                     cfg['name'] + '_epoch_' +
+                                     str(epoch) + '.pth')
             epoch += 1
 
         load_t0 = time.time()
@@ -125,6 +204,13 @@ def train():
         # forward
         out = net(images)
 
+        # Push images to comet
+        image_push = push_images(images[0].unsqueeze(0).cuda(), priors, 
+                                out[0][0].unsqueeze(0).cuda(), 
+                                out[1][0].unsqueeze(0).cuda(), 
+                                out[2][0].unsqueeze(0).cuda())
+        experiment.log_image(image_push, name="train_prediction",
+                            image_channels="last", step=iteration)
         # backprop
         optimizer.zero_grad()
         loss_l, loss_c, loss_landm = criterion(out, priors, targets)
@@ -137,8 +223,15 @@ def train():
         print('Epoch:{}/{} || Epochiter: {}/{} || Iter: {}/{} || Loc: {:.4f} Cla: {:.4f} Landm: {:.4f} || LR: {:.8f} || Batchtime: {:.4f} s || ETA: {}'
               .format(epoch, max_epoch, (iteration % epoch_size) + 1,
               epoch_size, iteration + 1, max_iter, loss_l.item(), loss_c.item(), loss_landm.item(), lr, batch_time, str(datetime.timedelta(seconds=eta))))
+        experiment.log_metric("Localization_Loss", loss_l.item(),
+                             step=iteration, epoch=epoch)
+        experiment.log_metric("Classification_Loss", loss_c.item(), 
+                             step=iteration, epoch=epoch)
+        experiment.log_metric("Landmark_Loss", loss_landm.item(), 
+                             step=iteration, epoch=epoch)
 
     torch.save(net.state_dict(), save_folder + cfg['name'] + '_Final.pth')
+    experiment.log_model("RetinaFace", save_folder + cfg['name'] + '_Final.pth')
     # torch.save(net.state_dict(), save_folder + 'Final_Retinaface.pth')
 
 
