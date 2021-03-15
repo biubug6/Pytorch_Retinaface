0395f8fb99d672413a65cbba62811181d2e8d973
d <ledaiduongvnth@gmail.com>
2021-03-15 14:01:08 +0700

    first commit

--- data/wider_face.py
+++ data/wider_face.py
@@ -5,6 +5,19 @@ import torch
 import torch.utils.data as data
 import cv2
 import numpy as np
+from scipy.ndimage import rotate
+
+def cal_rot_cord(image_shape, rotated_img_shape, xy, angle):
+    if xy[0] == -1 or xy[1] == -1:
+        return np.array((-1, -1))
+    org_center = (np.array(image_shape[:2][::-1]) - 1) / 2.
+    rot_center = (np.array(rotated_img_shape[:2][::-1]) - 1) / 2.
+    org = xy - org_center
+    a = np.deg2rad(angle)
+    new = np.array([org[0] * np.cos(a) + org[1] * np.sin(a),
+                    -org[0] * np.sin(a) + org[1] * np.cos(a)])
+    return new + rot_center
+
 
 class WiderFaceDetection(data.Dataset):
     def __init__(self, txt_path, preproc=None):
@@ -45,26 +58,46 @@ class WiderFaceDetection(data.Dataset):
         annotations = np.zeros((0, 15))
         if len(labels) == 0:
             return annotations
+        angle = np.random.randint(0, 90)
+        # angle = 0
+        img_rotated = rotate(img, angle)
         for idx, label in enumerate(labels):
             annotation = np.zeros((1, 15))
+            pts = [(label[0], label[1]), (label[0] + label[2], label[1] + label[3]), (label[4], label[5]),
+                   (label[7], label[8]), (label[10], label[11]), (label[13], label[14]), (label[16], label[17])]
+            pts_rt = [cal_rot_cord(img.shape, img_rotated.shape, np.array(pt), angle) for pt in pts]
+            # # draw landmarks
+            # cv2.circle(img_rotated, (int(pts_rt[2][0]), int(pts_rt[2][1])), 3, (255, 0, 0), 3)
+            # cv2.circle(img_rotated, (int(pts_rt[3][0]), int(pts_rt[3][1])), 3, (255, 0, 0), 3)
+            # cv2.circle(img_rotated, (int(pts_rt[4][0]), int(pts_rt[4][1])), 3, (255, 0, 0), 3)
+            # cv2.circle(img_rotated, (int(pts_rt[5][0]), int(pts_rt[5][1])), 3, (255, 0, 0), 3)
+            # cv2.circle(img_rotated, (int(pts_rt[6][0]), int(pts_rt[6][1])), 3, (255, 0, 0), 3)
+            # # draw box
+            # cv2.circle(img_rotated, (int(pts_rt[0][0]), int(pts_rt[0][1])), 3, (0, 255, 0), 3)
+            # cv2.circle(img_rotated, (int(pts_rt[1][0]), int(pts_rt[1][1])), 3, (0, 255, 0), 3)
+            # img_rotated = cv2.resize(img_rotated, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+            # # cv2.rectangle(img_rotated, (int(pts_rt[0][0]), int(pts_rt[0][1])), (int(pts_rt[1][0]), int(pts_rt[1][1])), (255, 0, 0), 1)
+            # print(self.imgs_path[index])
+            # cv2.imshow(str(self.imgs_path[index]), img_rotated)
+            # cv2.waitKey(0)
             # bbox
-            annotation[0, 0] = label[0]  # x1
-            annotation[0, 1] = label[1]  # y1
-            annotation[0, 2] = label[0] + label[2]  # x2
-            annotation[0, 3] = label[1] + label[3]  # y2
+            annotation[0, 0] = pts_rt[0][0]  # x1
+            annotation[0, 1] = pts_rt[0][1]  # y1
+            annotation[0, 2] = pts_rt[1][0]  # x2
+            annotation[0, 3] = pts_rt[1][1]  # y2
 
             # landmarks
-            annotation[0, 4] = label[4]    # l0_x
-            annotation[0, 5] = label[5]    # l0_y
-            annotation[0, 6] = label[7]    # l1_x
-            annotation[0, 7] = label[8]    # l1_y
-            annotation[0, 8] = label[10]   # l2_x
-            annotation[0, 9] = label[11]   # l2_y
-            annotation[0, 10] = label[13]  # l3_x
-            annotation[0, 11] = label[14]  # l3_y
-            annotation[0, 12] = label[16]  # l4_x
-            annotation[0, 13] = label[17]  # l4_y
-            if (annotation[0, 4]<0):
+            annotation[0, 4] = pts_rt[2][0]  # l0_x
+            annotation[0, 5] = pts_rt[2][1]  # l0_y
+            annotation[0, 6] = pts_rt[3][0]  # l1_x
+            annotation[0, 7] = pts_rt[3][1]  # l1_y
+            annotation[0, 8] = pts_rt[4][0]  # l2_x
+            annotation[0, 9] = pts_rt[4][1]  # l2_y
+            annotation[0, 10] = pts_rt[5][0] # l3_x
+            annotation[0, 11] = pts_rt[5][1]  # l3_y
+            annotation[0, 12] = pts_rt[6][0]  # l4_x
+            annotation[0, 13] = pts_rt[6][1]  # l4_y
+            if (annotation[0, 4] < 0):
                 annotation[0, 14] = -1
             else:
                 annotation[0, 14] = 1
@@ -72,9 +105,9 @@ class WiderFaceDetection(data.Dataset):
             annotations = np.append(annotations, annotation, axis=0)
         target = np.array(annotations)
         if self.preproc is not None:
-            img, target = self.preproc(img, target)
+            img_rotated, target = self.preproc(img_rotated, target)
 
-        return torch.from_numpy(img), target
+        return torch.from_numpy(img_rotated), target
 
 def detection_collate(batch):
     """Custom collate fn for dealing with batches of images that have a different
