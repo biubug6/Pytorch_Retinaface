4828aa5cbe705d137b52c545cae74d489f51c578
d <ledaiduongvnth@gmail.com>
2022-01-28 10:29:15 +0700

    Change load data

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import math
 import torch
 import torch.optim as optim
-import torch.utils.data as data
+from torch.utils.data import DataLoader
 from data import WiderFaceDetection, detection_collate, preproc, config
 from layers.modules import MultiBoxLoss
 from layers.functions.prior_box import PriorBox
@@ -35,42 +35,21 @@ with torch.no_grad():
 
 def train():
     net.train()
-    epoch = 0
-
     dataset = WiderFaceDetection(training_dataset, preproc(img_dim, rgb_mean))
-
-    epoch_size = math.ceil(len(dataset) / batch_size)
-    max_iter = max_epoch * epoch_size
-
-    for iteration in range(0, max_iter):
-        if iteration % epoch_size == 0:
-            # create batch iterator
-            batch_iterator = iter(data.DataLoader(dataset, batch_size, shuffle=True, num_workers=num_workers,
-                                                  collate_fn=detection_collate))
-            if epoch % 10 == 0 and epoch > 0:
-                torch.save(net.state_dict(), save_folder + config['name'] + '_epoch_' + str(epoch) + '.pth')
-            epoch += 1
-
+    train_loader = DataLoader(dataset, batch_size, shuffle=True, num_workers=num_workers, collate_fn=detection_collate)
+    for epoch in range(max_epoch):
+        for iter_index, (images, targets) in enumerate(train_loader):
+            images = images.cuda()
+            targets = [anno.cuda() for anno in targets]
+            out = net(images)
+            optimizer.zero_grad()
+            loss_l, loss_c, loss_landm = criterion(out, priors, targets)
+            loss = config['loc_weight'] * loss_l + loss_c + loss_landm
+            loss.backward()
+            optimizer.step()
         scheduler.step()
-        # load train data
-        images, targets = next(batch_iterator)
-        images = images.cuda()
-        targets = [anno.cuda() for anno in targets]
-
-        # forward
-        out = net(images)
-
-        # backprop
-        optimizer.zero_grad()
-        loss_l, loss_c, loss_landm = criterion(out, priors, targets)
-        loss = config['loc_weight'] * loss_l + loss_c + loss_landm
-        loss.backward()
-        optimizer.step()
-        print('Epoch:{}/{} || Epochiter: {}/{} || Iter: {}/{} || Loc: {:.4f} Cla: {:.4f} Landm: {:.4f}'
-              .format(epoch, max_epoch, (iteration % epoch_size) + 1,
-                      epoch_size, iteration + 1, max_iter, loss_l.item(), loss_c.item(), loss_landm.item()))
-
-    torch.save(net.state_dict(), save_folder + config['name'] + '_Final.pth')
+        print('Epoch:{} Loc: {:.4f} Cla: {:.4f} Landm: {:.4f}'.format(epoch, loss_l.item(), loss_c.item(), loss_landm.item()))
+        torch.save(net.state_dict(), save_folder + config['name'] + '_Final.pth')
 
 
 if __name__ == '__main__':
