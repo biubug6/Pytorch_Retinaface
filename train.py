12c147fbf7d9f99342d50f9789bf967a52357c51
d <ledaiduongvnth@gmail.com>
2022-02-08 16:08:57 +0700

    refactor

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import torch
 import torch.optim as optim
-from config import config
+from config import config as cfg
 # TODO improve Widerface dataset
 from dataset.wider_face import WiderFaceDetection, detection_collate
 # preproc is the function for performing image augmentation and preprocessing before training
@@ -17,11 +17,9 @@ from models.retinaface import RetinaFace
 
 rgb_mean = (104, 117, 123)
 num_classes = 2
-img_size = config['image_size']
-num_gpu = config['ngpu']
-batch_size = config['batch_size']
-max_epoch = config['epoch']
-gpu_train = config['gpu_train']
+img_size = cfg['image_size']
+batch_size = cfg['batch_size']
+max_epoch = cfg['epoch']
 dataset_path = 'data/train/label.txt'
 save_folder = './weights/'
 num_workers = 4
@@ -30,13 +28,13 @@ weight_decay = 5e-4
 initial_lr = 1e-3
 gamma = 0.1
 
-net = RetinaFace(cfg=config).cuda()
+net = RetinaFace(cfg=cfg).cuda()
 optimizer = optim.SGD(net.parameters(), lr=initial_lr, momentum=momentum, weight_decay=weight_decay)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=40, gamma=0.5)
 criterion = MultiBoxLoss(num_classes, 0.35, True, 0, True, 7, 0.35, False)
 
 from utils.prior_box import PriorBox
-priorbox = PriorBox(config, image_size=(img_size, img_size))
+priorbox = PriorBox(cfg, image_size=(img_size, img_size))
 with torch.no_grad():
     priors = priorbox.forward()
     priors = priors.cuda()
@@ -61,14 +59,14 @@ def train():
             out = net(images)
             optimizer.zero_grad()
             loss_l, loss_c, loss_landm = criterion(out, priors, targets)
-            loss = config['loc_weight'] * loss_l + loss_c + loss_landm
+            loss = cfg['box_weight'] * loss_l + cfg["class_weight"] * loss_c + cfg["landmark_weight"]*loss_landm
             loss.backward()
             optimizer.step()
             print('Epoch:{} Loc: {:.4f} Cla: {:.4f} Landm: {:.4f}'.format(epoch, loss_l.item(), loss_c.item(),
                                                                           loss_landm.item()))
         scheduler.step()
-        torch.save(net.state_dict(), save_folder + config['name'] + '_epoch_{}.pth'.format(str(epoch)))
-    torch.save(net.state_dict(), save_folder + config['name'] + '_Final.pth')
+        torch.save(net.state_dict(), save_folder + cfg['name'] + '_epoch_{}.pth'.format(str(epoch)))
+    torch.save(net.state_dict(), save_folder + cfg['name'] + '_Final.pth')
 
 
 if __name__ == '__main__':
